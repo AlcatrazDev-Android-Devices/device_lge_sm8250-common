91ce5a0b8450f86adc1aca9ad869d34f4fe7a0b4
1582130940 <19632674+1582130940@users.noreply.github.com>
2023-10-01 13:08:50 +0800

    sdm660-common: rootdir: Configure zram from fstab.zram
    * Don't configure zram in QCOM's init post boot script
    Volodymyr Zhdanov <wight554@gmail.com>
    
    * Tune zram performance
    Wei Wang <wvw@google.com>
    
    * Change zram swap size and swappiness
    Minchan Kim <minchan@google.com>
    
    * Set ZRAM to half of the accessible system RAM
    SamarV-121 <samarvispute121@gmail.com>
    
    * Remove max ZRAM compression streams setting
    Georg Veichtlbauer <georg@vware.at>
    
    * Drop usage of SWAP
    hridaya <info.hridayaprajapati@gmail.com>
    
    Co-authored-by: Volodymyr Zhdanov <wight554@gmail.com>
    Co-authored-by: Wei Wang <wvw@google.com>
    Co-authored-by: Minchan Kim <minchan@google.com>
    Co-authored-by: SamarV-121 <samarvispute121@gmail.com>
    Co-authored-by: Georg Veichtlbauer <georg@vware.at>
    Co-authored-by: hridaya <info.hridayaprajapati@gmail.com>
    Change-Id: Ifb134ff1eaf94a80dec1579afb91b0db30eceda8

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -946,10 +946,10 @@ low_ram=`getprop ro.config.low_ram`
 
 if [ "$ProductName" == "msmnile" ] || [ "$ProductName" == "kona" ] || [ "$ProductName" == "sdmshrike_au" ]; then
       # Enable ZRAM
-      configure_zram_parameters
+      # configure_zram_parameters
       configure_read_ahead_kb_values
       echo 0 > /proc/sys/vm/page-cluster
-      echo 100 > /proc/sys/vm/swappiness
+      # echo 100 > /proc/sys/vm/swappiness
 else
     arch_type=`uname -m`
 
@@ -1055,7 +1055,7 @@ else
     # Set allocstall_threshold to 0 for all targets.
     # Set swappiness to 100 for all targets
     echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
-    echo 100 > /proc/sys/vm/swappiness
+    # echo 100 > /proc/sys/vm/swappiness
 
     # Disable wsf for all targets beacause we are using efk.
     # wsf Range : 1..1000 So set to bare minimum value 1.
@@ -1067,7 +1067,7 @@ else
 
     configure_read_ahead_kb_values
 
-    enable_swap
+    # enable_swap
 fi
 }
 
@@ -4707,7 +4707,6 @@ case "$target" in
 
             # Turn on sleep modes.
             echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
-            echo 100 > /proc/sys/vm/swappiness
             ;;
         esac
     ;;
@@ -5263,7 +5262,6 @@ case "$target" in
 	echo N > /sys/module/lpm_levels/L3/l3-dyn-ret/idle_enabled
         # Turn on sleep modes.
         echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
-	echo 100 > /proc/sys/vm/swappiness
 	echo 120 > /proc/sys/vm/watermark_scale_factor
     ;;
 esac
