0133c255d0a661e92b0177493cbd3a6104dc2d26
Luca Stefani <luca.stefani.ge1@gmail.com>
2023-12-08 20:34:18 +0100

    sm8150-common: Enabled updateable APEX
    Change-Id: Iae46cbf48ae5fbfa100e83e8d6ce19f225102cf3

--- sm8150.mk
+++ sm8150.mk
@@ -4,5 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/lge/sm8150-common/sm8150-common-vendor.mk)
