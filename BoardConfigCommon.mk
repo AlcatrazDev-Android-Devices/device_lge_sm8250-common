3d6925b24da42d2b3a6eb45905e8902672a852a5
jmpfbmx <jmpf.bmx@gmail.com>
2023-12-08 18:28:58 +0100

    sm8150-common: Initial skeleton device tree
    Change-Id: Ia259c489f63cbc5912d19328ab9b3820eea2e512
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/lge/sm8150-common
+
+# Inherit the proprietary files
+include vendor/lge/sm8150-common/BoardConfigVendor.mk
---
+++ sm8150.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/lge/sm8150-common/sm8150-common-vendor.mk)
