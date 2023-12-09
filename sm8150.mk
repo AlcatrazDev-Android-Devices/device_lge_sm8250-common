320d5405b8c219fa1c3d6258f0c9aab9febfa0d3
jmpfbmx <jmpf.bmx@gmail.com>
2023-12-09 11:41:57 +0100

    sm8150-common: Build QTI Framework Detect libs
    Change-Id: Ib992e26a7a6e800adfebee1785de0c76a263b89a
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- sm8150.mk
+++ sm8150.mk
@@ -312,6 +312,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# QMI
+PRODUCT_PACKAGES += \
+    libjson \
+    libqti_vndfwk_detect \
+    libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti \
+    libvndfwk_detect_jni.qti.vendor
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.qcom
