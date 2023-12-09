3ea640a9c5815e5243f40e0f72d576a9747fb70a
jmpfbmx <jmpf.bmx@gmail.com>
2023-12-09 11:51:04 +0100

    sm8150-common: Build WiFi stack
    Change-Id: I17ab7921e03a4c2181fca82077ef54061fe6d71c
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- sm8150.mk
+++ sm8150.mk
@@ -414,7 +414,23 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    WifiOverlay
+    android.hardware.wifi@1.0-service \
+    vendor.qti.hardware.wifi.hostapd@1.0.vendor \
+    vendor.qti.hardware.wifi.hostapd@1.1.vendor \
+    vendor.qti.hardware.wifi.hostapd@1.2.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.0.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.1.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.2.vendor \
+    hostapd \
+    libwifi-hal-ctrl \
+    libwifi-hal-qcom \
+    libwpa_client \
+    WifiOverlay \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+PRODUCT_CFI_INCLUDE_PATHS += \
+    hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
