4d1422139196b9b33fc2fd1ffd4b97d13b21c5b5
jmpfbmx <jmpf.bmx@gmail.com>
2023-12-09 11:32:05 +0100

    sm8150-common: Build Camera stack
    Change-Id: Idc57d2090f78663aa0b0f7289a086a9d05809f16
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- sm8150.mk
+++ sm8150.mk
@@ -95,6 +95,15 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@2.0.vendor \
     liba2dpoffload
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    libcamera2ndk_vendor \
+    libdng_sdk.vendor \
+    libgui_vendor
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
