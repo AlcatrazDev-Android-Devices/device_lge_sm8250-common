cd85b197db8051678ded6bac2c724edfa6154866
jmpfbmx <jmpf.bmx@gmail.com>
2023-12-09 11:49:07 +0100

    sm8150-common: Build LGE Vibrator HIDL HAL
    Change-Id: I80484509f97c5b3bc3a766f33c5d8d5b8fb5f021
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- sm8150.mk
+++ sm8150.mk
@@ -408,6 +408,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.3-service.lge
+
 # Wifi
 PRODUCT_PACKAGES += \
     WifiOverlay
