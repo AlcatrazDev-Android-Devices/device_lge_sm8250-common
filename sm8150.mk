21286e69491d85fee3af9b25c89bb10ca02ca433
SGCMarkus <markusornik@gmail.com>
2023-12-10 11:20:26 +0100

    sm8150-common: add Light-HAL
    * kanged from judypn
    
    Change-Id: Ia1320f9518fb9226e9b3fbc9a49bf3803ab8d4aa

--- sm8150.mk
+++ sm8150.mk
@@ -228,6 +228,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.lge_sm8150
+
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.c2@1.0.vendor \
