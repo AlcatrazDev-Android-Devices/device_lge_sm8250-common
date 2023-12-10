4e34f357e91817564b965d388f8d2f2e83b4d832
jmpfbmx <jmpf.bmx@gmail.com>
2023-12-10 14:46:19 +0100

    sm8150-common: Include Dalvik Heap Configuration
    Change-Id: I89fb8a20aac0fc7870a2f6946b6ef6f3b7f0b452
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- sm8150.mk
+++ sm8150.mk
@@ -7,5 +7,8 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Dalvik Heap Configuration
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/lge/sm8150-common/sm8150-common-vendor.mk)
