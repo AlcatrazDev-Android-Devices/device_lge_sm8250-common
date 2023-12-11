ec216571df3f068fcaf1ee49f731fb9a8e9a6bf4
jmpfbmx <jmpf.bmx@gmail.com>
2023-12-11 16:38:50 +0100

    sm8150-common: Build Gatekeeper HIDL HAL
    Change-Id: I13d82e26a547354d7f9a4e5696903576299d89e7
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- sm8150.mk
+++ sm8150.mk
@@ -141,6 +141,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm@1.4-service.clearkey
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0.vendor
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
