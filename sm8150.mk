b899d191ad2da1f5340910767f181ca283718c80
LuK1337 <priv.luk@gmail.com>
2023-12-18 23:31:56 +0100

    sm8150-common: Build android.frameworks.sensorservice@1.0
    Required by mm-pp-deps.
    
    Change-Id: Iaf821dcbb07d7820f2fc3ff5ca9cca5e973360f2

--- sm8150.mk
+++ sm8150.mk
@@ -430,6 +430,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@2.0-service.multihal \
     libsensorndkbridge
 
