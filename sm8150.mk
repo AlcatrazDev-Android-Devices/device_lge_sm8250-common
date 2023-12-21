772e1b24be1fb49def63e4b6ea6dc0fa2add99d6
jmpfbmx <jmpf.bmx@gmail.com>
2023-12-21 09:55:18 +0100

    sm8150-common: Build libqti-perfd-client
    Change-Id: Ia56c8e17d57937b24a3cab5a13d819d3d0738635
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- sm8150.mk
+++ sm8150.mk
@@ -359,7 +359,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lge-libperfmgr
+    android.hardware.power-service.lge-libperfmgr \
+    libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
