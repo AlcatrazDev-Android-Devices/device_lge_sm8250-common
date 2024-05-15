ee820ffc1f3c0b4d026da3aace2bf86a6978fc10
Alcatraz323 <alcatraz32323@gmail.com>
2024-05-15 19:35:29 +0800

    [DNM] sn8250-common: set permissive
    Change-Id: I6fcc31341b4e6651de537c8821cf29807bd3df9e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -110,7 +110,7 @@ BOARD_KERNEL_CMDLINE := \
     kpti=off \
     cgroup.memory=nokmem,nosocket \
     androidboot.init_fatal_reboot_target=recovery 
-# BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
