e13186343d86a3dc73359088b8ab7d2201e2b91a
Alcatraz323 <alcatraz32323@gmail.com>
2024-05-16 21:21:58 +0800

    sm8250-common: remove PRODUCT_CFI_INCLUDE_PATHS for wpa_supplicant
    * defined in vendor/lineage
    
    Change-Id: Ie60856ba3305e043394fe818ea484f48ee1f7839

--- sm8250.mk
+++ sm8250.mk
@@ -527,9 +527,6 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
-PRODUCT_CFI_INCLUDE_PATHS += \
-    hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
