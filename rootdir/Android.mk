86529e4c43cb4a518325c375653e56519fef2f08
Alcatraz323 <alcatraz32323@gmail.com>
2024-05-28 13:13:18 +0800

    sm8250-common: use source build libs if available & clean up duplicated recipes
    Change-Id: Id90a55a9fd0433ecfcb7614d1e4012380873d172

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -245,14 +245,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.time_daemon.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.time_daemon.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.vendor.sensors.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
