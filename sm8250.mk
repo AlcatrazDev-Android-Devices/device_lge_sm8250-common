46cd758ee321bd1471a3fda0ae76b9e0a1f86356
Alcatraz323 <alcatraz32323@gmail.com>
2024-05-28 13:13:18 +0800

    sm8250-common: use source build libs if available & clean up duplicated recipes
    Change-Id: Id90a55a9fd0433ecfcb7614d1e4012380873d172

--- sm8250.mk
+++ sm8250.mk
@@ -72,10 +72,12 @@ PRODUCT_PACKAGES += \
     libaudiopreprocessing \
     libbatterylistener \
     libbundlewrapper \
+    libcirrusspkrprot \
     libcomprcapture \
     libdownmix \
     libdynproc \
     libeffectproxy \
+    libexthwplugin \
     libhdmiedid \
     libhfp \
     libldnhncr \
@@ -84,6 +86,7 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     libreverbwrapper \
     libsndmonitor \
+    libspkrprot \
     libtinycompress \
     libvisualizer \
     libvolumelistener \
@@ -126,6 +129,7 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    vendor.qti.hardware.camera.postproc@1.0.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
@@ -155,11 +159,15 @@ PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
     libqdMetaData \
     libqdMetaData.system \
+    libsdedrm \
+    libsdmcore \
     libtinyxml \
     libvulkan \
     memtrack.kona \
     vendor.display.config@1.16 \
     vendor.display.config@2.0 \
+    vendor.display.config@1.16.vendor \
+    vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.allocator@1.0.vendor \
     vendor.qti.hardware.display.allocator@3.0.vendor \
@@ -307,7 +315,8 @@ PRODUCT_PACKAGES += \
     libchrome.vendor \
     NfcNci \
     SecureElement \
-    Tag
+    Tag \
+    vendor.nxp.nxpnfc@2.0.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
@@ -436,7 +445,6 @@ PRODUCT_PACKAGES += \
     init.qti.fm.rc \
     init.target.rc \
     init.target.wigig.rc \
-    init.time_daemon.rc \
     init.vendor.sensors.rc \
     ueventd.qcom.rc
 
