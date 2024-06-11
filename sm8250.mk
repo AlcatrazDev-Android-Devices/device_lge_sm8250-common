f0151ccc1b89d3d82421924f22fa26c96af21405
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-06-11 14:53:01 +0800

    sm8250-common: lights: import from xiaomi Light AIDL
    Co-authored-by: BeYkeRYkt <beykerykt@gmail.com>
    Co-authored-by: Jiyong Park <jiyong@google.com>
    Co-authored-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Change-Id: I80ae39a49ae4d9ce589a1f048eb87bff96ade98e

--- sm8250.mk
+++ sm8250.mk
@@ -264,6 +264,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.lge_sm8250
+
 # Livedisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service.lge
