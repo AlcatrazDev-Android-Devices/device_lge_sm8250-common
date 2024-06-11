13453f196ffcfa41b3d98e3d8348389c539c5d90
Han Sol Jin <hansol@hansol.ca>
2024-06-11 13:15:49 +0800

    sm8250-common: remove Bluetooth HIDL impl * Already building the AIDL impl
    Change-Id: I621d9fcd48ca17cedaede0d9433a09b201e10c5a

--- sm8250.mk
+++ sm8250.mk
@@ -60,7 +60,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl \
     android.hardware.bluetooth.audio-impl \
-    android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
